6cc189e31afa3cfb3f7c2d5f6e2ce5d112e9232c
21608 <oka00027@gmail.com>
2023-09-23 22:48:48 +0900

    switch文の結果を変数に代入してみた

--- java14/src/java14/SwitchMain.java
+++ java14/src/java14/SwitchMain.java
@@ -3,13 +3,22 @@ package java14;
 public class SwitchMain {
 
 	public static void main(String[] args) {
-		String str = "one";
+		String one = "one";
+		String two = "two";
 
 		System.out.println(
-			switch(str) {
-				case "one" -> "one";
+			switch(one) {
+				case "one" -> one;
 				default    -> "default";
 			}
 		);
+
+		String switchStr = switch(two) {
+			case "one" -> one;
+			case "two" -> two;
+			default -> throw new IllegalArgumentException("Unexpected value");
+		};
+
+		System.out.println(switchStr);
 	}
 }
